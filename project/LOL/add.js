70d7b4922be2adcdc0430978a1aa275696981a5c
shuaiyiji <shuaiyiji@sina.com>
2016-09-15 21:16:23 +0800

    自己学习的小游戏
    自己学习的小游戏

---
+++ project/LOL/add.js
@@ -0,0 +1,157 @@
+
+function addLoadEvent(func){
+    var b = window.onload;
+    if(typeof window.onload != "function"){
+        window.onload = func;
+    }else{
+        window.onload = function () {
+            b();
+            func();
+        }
+    }
+}
+function addclass(elment,claasName){
+    if(!elment.className){
+        elment.className = claasName;
+    }else{
+        var a = elment.className;
+        a += " ";
+        a += claasName;
+        elment.className = a;
+    }
+}
+
+function move(ID,x,y,time){
+    var a=document.getElementById(ID);
+    if(!a.style.top) a.style.top="0px";
+    if(!a.style.left) a.style.left="0px";
+    var ax=parseInt(a.style.left);
+    var ay=parseInt(a.style.top);
+    if(a.move) clearTimeout(a.move);
+    if(ax<x) ax+=Math.ceil((x-ax)/10);
+    if(ax>x) ax-=Math.ceil((ax-x)/10);
+    if(ay<y) ay+=Math.ceil((y-ay)/10);
+    if(ay>y) ay-=Math.ceil((ay-y)/10);
+    a.style.left=ax+"px";
+    a.style.top=ay+"px";
+    a.move=setTimeout("move('"+ID+"',"+x+","+y+","+time+")",time);
+}
+
+function insertAfter(newElement,old){
+    var parent=old.parentNode;
+    if(parent.lastChild==old){
+        parent.appendChild(newElement);
+    }else{
+        parent.insertBefore(newElement,old.nextSibling);
+    }
+}
+
+function a(){
+    if(!document.getElementById("ms")) return false;
+    var links = document.getElementsByTagName("a");
+    var ms = document.getElementById("ms");
+    console.log(links.length);
+    for(var i = 0;i<links.length;i++){
+        links[i].onmouseover = function () {
+           var href = this.getAttribute("href");
+            //alert(href);
+            if(href.indexOf("index.html") != -1)
+                move("ms",0,0,20);
+            if(href.indexOf("jiqiao.html") != -1)
+                move("ms",-150,0,20);
+            if(href.indexOf("jineng.html") != -1)
+                move("ms",-300,0,20);
+            if(href.indexOf("paihang.html") != -1)
+                move("ms",-450,0,20);
+            if(href.indexOf("lianxi.html") != -1)
+                move("ms",-600,0,20);
+
+
+        }
+    }
+    //return true;
+}
+function b(){
+    var links = document.getElementsByTagName('a');
+    var hide = document.getElementsByClassName("hide");
+    for(var i = 0;i<links.length;i++){
+        links[i].onclick = function () {
+            var id = this.getAttribute("href").split("#")[1];
+            for(var j = 0;j<hide.length;j++){
+                if(id == hide[j].getAttribute("id")){
+                    hide[j].style.display = "block";
+                }else{
+                    hide[j].style.display = "none";
+                }
+            }
+        }
+    }
+}
+
+function c(){
+    if(!document.getElementsByTagName("tbody")) return false;
+    if(!document.getElementById("paihang")) return false;
+    var main = document.getElementById("paihang");
+    var tb = document.getElementsByTagName("tbody")[0];
+    var tr = tb.getElementsByTagName("tr");
+    var span = tb.getElementsByTagName("span");
+    console.log(span.length);
+    for(var i = 0;i<tr.length;i++){
+        if(i%2 != 0){
+            addclass(tr[i],"even")
+        }else{
+            addclass(tr[i],"odd");
+        }
+    }
+    var dl = document.createElement("dl");
+    var p = document.createElement("h3");
+    var p_text = document.createTextNode("名词解释");
+    p.appendChild(p_text);
+    main.appendChild(p)
+    for(var j = 0;j<span.length;j++){
+        var dd = document.createElement("dd");
+        var dt = document.createElement("dt");
+        var title = span[j].getAttribute("title");
+        var text = span[j].lastChild.nodeValue;
+        dd.innerHTML = text;
+        dt.innerHTML = title;
+        //dd.appendChild(text);
+        //dt.appendChild(title);
+        dl.appendChild(dd);
+        dl.appendChild(dt);
+    }
+    main.appendChild(dl);
+
+}
+
+
+function g(){
+    if(!document.getElementById("jineng")) return false;
+    var jn = document.getElementById("jineng");
+    var p = document.createElement("p");
+    var img = document.createElement("img");
+    var p_text = document.createTextNode("选择小技能图片");
+    p.appendChild(p_text);
+    jn.appendChild(p);
+    //insertAfter()
+    img.setAttribute("src","images/placeholder.gif");
+    img.setAttribute("class","pic");
+    jn.appendChild(img);
+    var links = jn.getElementsByTagName("a");
+    //console.log(links.length);
+    for(var i = 0;i<links.length;i++){
+        links[i].onclick = function(){
+            var title =this.title;
+            var href = this.href;
+            p.textContent = title;
+            img.setAttribute("src",href);
+            return false;
+        }
+    }
+}
+
+//a();
+addLoadEvent(a);
+addLoadEvent(b);
+addLoadEvent(c);
+addLoadEvent(g);
---
+++ project/九宫格游戏/js.js
@@ -0,0 +1,160 @@
+/**
+ * Created by jishuaiyi on 16/9/6.
+ */
+
+$(document).ready(function () {
+    change();
+});
+var SUCCESS = [1,2,3,4,5,6,7,8,0];
+var emptyPosition; // 空格的位置
+var randomPosition = [];
+var step = 0;
+function change(){
+    var index = parseInt(Math.random()*9)+1;
+    $('.block,.answer').css('background-image','url(img/bg_'+index+'.jpg)');
+    randomPlace();
+    //return 1;
+
+}
+
+function randomPlace(){
+        console.log(this);
+    //$('.step').html(step);
+    var arr = $.extend([],SUCCESS);
+    for(var x, j,i = arr.length;i;j = parseInt(Math.random()*i),x = arr[--i],arr[i] = arr[j],arr[j] = x);
+    console.log(arr)
+    randomPosition = arr;
+    console.log(arr)
+    for(var i in arr){
+        if(arr[i] == 0){
+            //console.log(i);
+            emptyPosition = parseInt(i);
+        }
+        console.log(arr[i]);
+        $("#main>div").eq(arr[i]).attr('data-index',i).css({
+            left : 100*(i%3),
+            top : 100*(Math.floor(i/3))
+        })
+    }
+}
+//更新空位置
+function upDataPosition(p1){
+
+    emptyPosition = p1;
+    step = step + 1;
+    var tmp = [];
+    var end = ['8','0','1','2','3','4','5','6','7'];
+    $('#main>div').each(function () {
+        tmp.push($(this).attr('data-index'));
+    })
+    if(tmp.toString() == end.toString()){
+        $('#success').show();
+    }
+    $('.step').html(step);
+}
+
+function left(){
+    if(emptyPosition%3<2){
+        var currentIndex = parseInt(emptyPosition)+1;
+        $('#main>div[data-index=' + currentIndex + ']').animate({
+            left :'-=100px'
+        },100, function () {
+            $(this).attr('data-index',currentIndex-1);
+        });
+
+        $('#main>div[data-index='+emptyPosition+']').animate({
+            left  :'+=100px'
+        },100, function () {
+            $(this).attr('data-index',currentIndex);
+            upDataPosition(currentIndex);
+        })
+
+    }
+
+}
+function right(){
+    if(emptyPosition%3>0){
+        var currentIndex = parseInt(emptyPosition)-1;
+        $('#main>div[data-index=' + currentIndex + ']').animate({
+            left :'+=100px'
+        },100, function () {
+            $(this).attr('data-index',currentIndex+1);
+        });
+
+        $('#main>div[data-index='+emptyPosition+']').animate({
+            left  :'-=100px'
+        },100, function () {
+            $(this).attr('data-index',currentIndex);
+            upDataPosition(currentIndex);
+        })
+    }
+}
+
+function up(){
+    if(emptyPosition<6){
+        var currentIndex = parseInt(emptyPosition)+3;
+        $('#main>div[data-index='+currentIndex+']').animate({
+            top : '-=100px'
+        },100,function(){
+            $(this).attr('data-index',currentIndex-3);
+        })
+        $('#main>div[data-index='+emptyPosition+']').animate({
+            top : '+=100px'
+        },100, function () {
+            $(this).attr('data-index',currentIndex);
+            upDataPosition(currentIndex);
+         })
+
+    }
+}
+function down(){
+    if(emptyPosition > 2){
+        var currentIndex = parseInt(emptyPosition)-3;
+        $('#main>div[data-index='+currentIndex+']').animate({
+            top : '+=100px'
+        },100,function(){
+            $(this).attr('data-index',currentIndex+3);
+        })
+        $('#main>div[data-index='+emptyPosition+']').animate({
+            top : '-=100px'
+        },100, function () {
+            $(this).attr('data-index',currentIndex);
+            upDataPosition(currentIndex);
+        })
+    }
+}
+$(document).ready(function () {
+    change();
+}).on('keypress', function (e) {
+    console.log(e.keyCode)
+    switch (e.keyCode){
+        case 97:
+            console.log(6666);
+            left();
+            break;
+        case 100:
+            right();
+            break;
+        case 119:
+            up();
+            break;
+        case 115:
+            down();
+            break;
+    }
+}).on('click','#showAnswer', function () {
+    $('.answer').toggle()
+}).on('click','#showNum', function () {
+    $('#main>div>span').toggle()
+}).on('click','.reset', function () {
+    randomPlace();
+    $('#success').hide()
+}).on('click','#change', function () {
+    change()
+});
+
+touch.on(document,'swipeleft',left);
+touch.on(document,'swiperight',right);
+touch.on(document,'swipeup',up);
+touch.on(document,'swipedown',down);
+
